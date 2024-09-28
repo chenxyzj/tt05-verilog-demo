74fafa7a8dacdfd157bbae1f63c0b89099f25425
chenxyzj <26920259+chenxyzj@users.noreply.github.com>
2024-09-28 17:38:15 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -20,7 +20,7 @@ async def test_7seg(dut):
     dut.rst_n.value = 1
 
     # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
+    max_count = int(dut.ui_in.value) << 10
     dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
     # check all segments and roll over
     for i in range(15):
@@ -38,7 +38,7 @@ async def test_7seg(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    max_count = dut.ui_in.value << 10
+    max_count = int(dut.ui_in.value) << 10
     dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
     # check all segments and roll over
     for i in range(15):
